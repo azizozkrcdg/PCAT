d5132b189c765cb24a4217731e110d4a0bd15b2f
aziz özkaracadağ <azizozkaracadaag@gmail.com>
2024-11-11 20:12:56 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = () => {
-  console.log('ffff');
-};
+import express from 'express';
+
+const app = express();
+
+// MİDDLEWARES
+app.use(express.static('public'));
+
+// EJS TEMPLATE ENGİNE
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`The server is running on port ${port}`);
+});
