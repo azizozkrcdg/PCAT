6088cef6634e852b6f2bc5d47a5465b61b0f0e33
aziz özkaracadağ <azizozkaracadaag@gmail.com>
2024-11-29 20:14:22 +0300

    nc

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 import express from 'express';
-import ejs from "ejs";
-import Photo from "./models/Photo.js";
+import methodOverride from 'method-override';
+import ejs from 'ejs';
+import Photo from './models/Photo.js';
 import mongoose from 'mongoose';
+import fileUpload from 'express-fileupload';
+import fs from 'fs';
 
 const app = express();
 
 // connect db
-mongoose.connect("mongodb://localhost/pcat-db");
+mongoose.connect('mongodb://localhost/pcat-db');
 
 // MİDDLEWARES
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(methodOverride('_method'));
 
 // EJS TEMPLATE ENGİNE
 app.set('view engine', 'ejs');
 
+// ROUTES
+
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({})
+  const photos = await Photo.find({});
   res.render('index.ejs', {
-    photos
+    photos,
   });
 });
 
 app.get('/photos/:id', async (req, res) => {
   const photo = await Photo.findById(req.params.id);
-  res.render("photo", {photo});
+  res.render('photo', { photo });
 });
 
 app.get('/about', (req, res) => {
@@ -37,10 +44,37 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect("/");
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadImage = req.files.image;
+  let uploadPath = './public/uploads/' + uploadImage.name;
+
+  uploadImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadImage.name,
+    });
+    res.redirect('/');
+  });
 });
 
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findById({ _id: req.params.id });
+  res.render('edit.ejs', { photo });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findById({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+
+  res.redirect(`/photos/${req.params.id}`);
+});
 
 // port listen
 const port = 3000;
